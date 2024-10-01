f8de2d7d138ed987a2116c9568fbed483399d281
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-10-02 00:49:27 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-"""Square Class
+"""
+Square Class
 
-A Square Class
-
+This module defines a Square class that represents a square shape.
+It allows the creation of square objects with a specified size.
 """
 
-
 class Square:
+    """
+    A class that represents a square.
+
+    This class allows the creation of square objects by defining
+    their size, which is a positive integer. The class currently
+    does not have any methods to manipulate the square, but it 
+    serves as a foundation for further extensions.
+    """
 
     def __init__(self, size):
-        """__init__
+        """
+        Initialize a Square instance.
 
-        The __init__ method initializes the size value
-        of the square.
+        The __init__ method initializes the size value of the square.
 
         Attributes:
-            size (int): The size of the square.
-
+            size (int): The size of the square, which must be a 
+                         positive integer.
         """
         self.__size = size
