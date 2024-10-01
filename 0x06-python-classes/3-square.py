1dce9f1a7daf0b4d36e1ddf5e6542836d76882bc
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-10-02 01:19:02 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,9 +7,9 @@ class Square:
     def __init__(self, size=0):
         """Initialize a Square instance. The __init__ method initializes the size of the square.
         """
-        if type(size) is not int:
+        if not ininstance(size, size=0):
             raise TypeError('size must be an integer')
-        if size < 0:
+        elif size < 0:
             raise ValueError('size must be >= 0')
 
         self.__size = size
@@ -17,4 +17,4 @@ class Square:
     def area(self):
         """Calculate the area of the square. int: The area of the square, calculated as size squared.
         """
-        return self.__size ** 2
+        return (self.__size ** 2)
