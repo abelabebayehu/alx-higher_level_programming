332ebb061dfadfb08db7404946653fc9a27a1955
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-10-07 20:16:31 +0300

    Create 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+
+The Rectangle class is currently empty and serves as a placeholder for future development.
+
+"""
+
+class Rectangle:
+    """
+    An empty Rectangle class.
+
+    This class currently does not have any attributes or methods. It can be used as a
+    foundation for creating rectangle objects.
+    """
+    
+    pass
