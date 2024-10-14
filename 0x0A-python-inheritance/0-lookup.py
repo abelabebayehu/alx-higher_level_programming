90a6e0e0d514934202e2cff6858d78141cb500a5
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-10-14 22:29:02 +0300

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """
+    """Looks up object's attributes/methods
+    Args:
+        obj
     Returns the list of available attributes and methods of an object
     """
 
