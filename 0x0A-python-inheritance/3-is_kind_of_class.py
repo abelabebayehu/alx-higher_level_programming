4fae73a4650a3945c013c4aa289545ec09c40f80
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-10-14 22:46:46 +0300

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a class and inherited class-checking function."""
+
+
 def is_kind_of_class(obj, a_class):
     """
     Checks if `obj` is the same class or inherit from `a_class`
