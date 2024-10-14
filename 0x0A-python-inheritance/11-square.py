bbbe1d1eff4d59e436e87d4574cdd57b5512fdf7
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-10-14 23:12:38 +0300

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""a Rectangle subclass Square."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     def __init__(self, size):
+        """defines"""
         self.integer_validator(size, size)
         super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
+        """defines"""
         return '[Square] ' + str(self.__size) + '/' + str(self.__size)
