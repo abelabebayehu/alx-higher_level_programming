85619ae384fca80e683c7dcd3e2f55d986dbeba6
Abel Abebayehu <157596006+abelabebayehu@users.noreply.github.com>
2024-10-15 23:27:09 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+      """
+    Reads a text file (UTF8) and prints its content to the standard output.
+
+    Args:
+        filename (str): The name of the file to be read. Defaults to an empty string.
+    """
     with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
