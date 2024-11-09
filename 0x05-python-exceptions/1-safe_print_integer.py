6028b9433d43d7e10653892ae31537dd44e9c22d
abelabebayehu <abelperspectives@gmail.com>
2024-11-09 16:45:55 +0300

    added 1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+
+    try:
+        value = int(value)
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        print("please input an integer")
+        return False
