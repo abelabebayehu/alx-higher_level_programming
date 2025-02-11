2cb37dd5307d039281ef541173980a645dd5895f
abelabebayehu <abelperspectives@gmail.com>
2025-02-11 20:29:38 +0300

    added files for web scrapping project

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const apiData = JSON.parse(body);
+    const films = apiData.results;
+    const ID = 'https://swapi-api.alx-tools.com/api/films/';
+    let count = 0;
+    for (let i = 0; i < films.length; i++) {
+      const characters = films[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === ID) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,12 @@
+# 0x14. Javascript - Web scraping
+___
+### General learning objectives
+- How to manipulate JSON data
+- How to use request and fetch API
+- How to read and write a file using fs module
+
+### Tasks
+- Write a script that reads and prints the content of a file.
+- Write a script that writes a string to a file.
+- Write a script that display the status code of a GET request.
+- Write a script that prints the title of a Star Wars movie where the episode number matches a given integer.
