4b8de19303048b58e49501ba895d6bfac27cb923
abelabebayehu <abelperspectives@gmail.com>
2025-02-19 18:22:26 +0300

    added files

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (films) {
+  $.each(films.results, function (data, movies) {
+    $('#list_movies').append('<li>' + movies.title + '</li>');
+  });
+}, 'json'
+);
